408ca7513b75e579675ffce53b92e0441cfb1ed2
miguel-ll <100251728+miguel-ll@users.noreply.github.com>
2022-10-10 01:47:57 -0300

    Add files via upload

---
+++ ackerman.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ack(int m, int n) {
+	int ans;
+	if (m == 0) ans = n+1;
+	else if (n == 0) ans = ack(m-1, 1);
+	else ans = ack(m-1, ack(m, n-1));
+	return ans;
+}
+
+int main(void) {
+	int i, j;
+	for(i=0; i<=6; i++) {
+		for(j=0; j<=6; j++) {
+	    printf("ackerman (%d, %d) is: %d\n", i, j, ack(i,j));
+} } }
