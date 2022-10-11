ca44f424e554cf66618b64363f43ff6668e5598f
miguel-ll <100251728+miguel-ll@users.noreply.github.com>
2022-10-11 19:31:02 -0300

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,3 +1,36 @@
+# for string sorting
+def countSort(arr):
+    # The output character array that will have sorted arr
+    output = [0 for i in range(len(arr))]
+  
+    # Create a count array to store count of individual
+    # characters and initialize count array as 0
+    count = [0 for i in range(256)]
+  
+    # For storing the resulting answer since the 
+    # string is immutable
+    ans = ["" for _ in arr]
+  
+    # Store count of each character
+    for i in arr:
+        count[ord(i)] += 1
+  
+    # Change count[i] so that count[i] now contains actual
+    # position of this character in output array
+    for i in range(256):
+        count[i] += count[i-1]
+  
+    # Build the output character array
+    for i in range(len(arr)):
+        output[count[ord(arr[i])]-1] = arr[i]
+        count[ord(arr[i])] -= 1
+  
+    # Copy the output array to arr, so that arr now
+    # contains sorted characters
+    for i in range(len(arr)):
+        ans[i] = output[i]
+    return ans 
+
 def heapify(arr, N, i):
     largest = i  # Initialize largest as root
     l = 2 * i + 1     # left = 2*i + 1
