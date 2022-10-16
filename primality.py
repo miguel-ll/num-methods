126d88df5c18efae80b363c52cb668c71c74fd4d
miguel-ll <100251728+miguel-ll@users.noreply.github.com>
2022-10-15 22:03:50 -0300

    Delete primality.py

--- primality.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-# Fermat primality test
-def fermat_test(n):
-  for _ in range(100):
-    a = randint(2,n-2)
-    if pow(a,n-1,n) != 1:
-      return False
-  return True
